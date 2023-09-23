f962862ed07792f27fd017ab81ff3382b62201d0
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-23 21:52:33 +0300

    sorting_algorithms C0x0C

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,40 +7,40 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *loop_node;
-    listint_t *loop_check;
-    listint_t *tmp1;
-    listint_t *tmp2;
+	listint_t *loop_node;
+	listint_t *loop_check;
+	listint_t *tmp1;
+	listint_t *tmp2;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-    {
-        return;
-    }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
 
-    loop_node = (*list)->next;
-    while (loop_node != NULL)
-    {
-        loop_check = loop_node;
-        while (loop_check->prev != NULL && loop_check->prev->n > loop_check->n)
-        {
-            tmp1 = loop_check->prev;
-            tmp2 = loop_check;
+	loop_node = (*list)->next;
+	while (loop_node != NULL)
+	{
+		loop_check = loop_node;
+		while (loop_check->prev != NULL && loop_check->prev->n > loop_check->n)
+		{
+			tmp2 = loop_check;
+			tmp1 = loop_check->prev;
 
-            if (tmp2->next != NULL)
-                tmp2->next->prev = tmp1;
-            tmp1->next = tmp2->next;
-            tmp2->prev = tmp1->prev;
-            tmp1->prev = tmp2;
-            tmp2->next = tmp1;
+			if (tmp2->next != NULL)
+				tmp2->next->prev = tmp1;
+			tmp1->next = tmp2->next;
+			tmp2->prev = tmp1->prev;
+			tmp1->prev = tmp2;
+			tmp2->next = tmp1;
 
-            if (tmp2->prev != NULL)
-                tmp2->prev->next = tmp2;
-            else
-                *list = tmp2;
+			if (tmp2->prev != NULL)
+				tmp2->prev->next = tmp2;
+			else
+				*list = tmp2;
 
-            loop_check = tmp2;
-            print_list(*list);
-        }
-        loop_node = loop_node->next;
-    }
+			loop_check = tmp2;
+			print_list(*list);
+		}
+		loop_node = loop_node->next;
+	}
 }
