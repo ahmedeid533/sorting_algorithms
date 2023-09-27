042c2a5c67a316f7995bf69995a86e5323c14f64
ahmedeid533 <ahmedeidgoma333@gmail.com>
2023-09-27 15:00:45 +0300

    sorting_algorithms C0x1

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -25,6 +25,7 @@ int partition(int *array, size_t low, size_t high)
     int pivot = array[high];
     int i = low - 1;
     size_t j;
+
     for (j = low; j < high; j++)
     {
         if (array[j] < pivot)
@@ -46,9 +47,11 @@ int partition(int *array, size_t low, size_t high)
  */
 void quick_sort_recursive(int *array, int low, int high)
 {
+    int pivot_index;
+
     if (low < high)
     {
-        int pivot_index = partition(array, low, high);
+        pivot_index = partition(array, low, high);
         quick_sort_recursive(array, low, pivot_index - 1);
         quick_sort_recursive(array, pivot_index + 1, high);
     }
@@ -56,7 +59,7 @@ void quick_sort_recursive(int *array, int low, int high)
 
 /**
  * quick_sort - Sorts an array of integers in ascending order using the
- *              quicksort algorithm.
+ * quicksort algorithm.
  * @array: The array to be sorted.
  * @size: The number of elements in the array.
  */
